9c5a042617d9ff6862ea1b4576279f1dd5b51364
Mira <mira.peryshko@metropolia.fi>
2024-11-06 23:36:49 +0200

    Refactor tour(s) component

--- src/components/Tour.jsx
+++ src/components/Tour.jsx
@@ -1,5 +1,32 @@
-function Tour() {
-  return <div>Tour</div>;
+const Tour = ({ image, date, title, info, location, duration, cost }) => {
+  return (
+    <article className='tour-card'>
+      <div className='tour-img-container'>
+        <img
+        src={image}
+        className="tour-img"
+        alt={title}
+        />
+        <p className="tour-date">{date}</p>
+      </div>
+      <div className='tour-info'>
+        <div className="tour-title">
+          <h4>{title}</h4>
+        </div>
+        <p>{info}</p>
+        <div className="tour-footer">
+          <p>
+            <span>
+              <i className="fas fa-map" />
+            </span>
+            {location}
+          </p>
+          <p>{cost}</p>
+          <p>{duration}</p>
+        </div>
+      </div>
+    </article>
+  )
 }
 
-export default Tour;
+export default Tour
